6ea395faeb234029ec43cd53d6447f95f325f762
Andrii <nikulin.and90@gmail.com>
2023-03-09 21:58:54 +0200

    add card hover

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
        <img width={40} height={40} src="/images/logo.png" alt="#" />
         <div>
           <h3 className="text-uppercace">REACT SNEAKERS</h3>
-          <p>Магазин лучших кроссовок</p>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
         </div>
        </div>
         <ul className="d-flex">
@@ -22,9 +22,18 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+        <h1> Все кроссовки</h1>
+        <div className="searh-block">
+          <img src="/images/search.svg" alt="Search"/>
+          <input placeholder="Search..." />
+          </div>
+        </div>
             <div className="d-flex">
             <div className="card">
+              <div className="favorite">
+                <img src="/images/heart-unliked.svg" alt="Unliked" />
+              </div>
           <img width={133} height={112} src="/images/sneacers/1.jpg" alt="" />
           <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
           <div className="d-flex justify-between align-center">
@@ -76,19 +85,7 @@ function App() {
           </button>
           </div>
         </div>
-        <div className="card">
-          <img width={133} height={112} src="/images/sneacers/5.jpg" alt="" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-          <div className="d-flex flex-column">
-            <span>Цена:</span>
-            <b>5999 грн</b>
-          </div>
-          <button className="button">
-            <img width={11} height={11} src="/images/plus.svg" alt="Plus" />
-          </button>
-          </div>
-        </div>
+       
             </div>
       </div>
     </div>
