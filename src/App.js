77b05d97771c769a702abc60b6385e165c012427
Andrii <nikulin.and90@gmail.com>
2023-03-13 15:04:04 +0200

    start logic react

--- src/App.js
+++ src/App.js
@@ -5,46 +5,105 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
       <div className="drawer">
-        <h2 className="mb-30">Корзина</h2>
+        <h2 className="d-flex justify-between mb-30 ">Корзина<img className="cu-p" src="/images/btn-remove.svg" alt="Remove"/></h2>
 
+     <div className="items">
         <div className="cartItem d-flex align-center mb-20">
          <div 
-        style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
-        className='cartItemImg'></div>
+          style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+          className='cartItemImg'></div>
 
       <div className="mr-20 flex">
             <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
             <b>5999 грн</b>
           </div>
-          <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
-          </div>
-
-          <div className="cartItem d-flex align-center">
-          <div 
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+        <div className="cartItem d-flex align-center mb-20">
+         <div 
           style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
           className='cartItemImg'></div>
+
+      <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>5999 грн</b>
+          </div>
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+        <div className="cartItem d-flex align-center mb-20">
+         <div 
+          style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+          className='cartItemImg'></div>
+
+      <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>5999 грн</b>
+          </div>
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+        <div className="cartItem d-flex align-center mb-20">
+         <div 
+          style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+          className='cartItemImg'></div>
+
+      <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>5999 грн</b>
+          </div>
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+        <div className="cartItem d-flex align-center mb-20">
+         <div 
+          style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+          className='cartItemImg'></div>
+
+      <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>5999 грн</b>
+          </div>
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+        <div className="cartItem d-flex align-center mb-20">
+         <div 
+          style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+          className='cartItemImg'></div>
+
+      <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>5999 грн</b>
+          </div>
+           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
+           </div>
+
+           <div className="cartItem d-flex align-center">
+           <div 
+             style={{backgroundImage: "url(/images/sneacers/1.jpg)"}} 
+             className='cartItemImg'></div>
 
           <div className="mr-20 flex">
             <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
             <b>5999 грн</b>
           </div>
           <img className="removeBtn" src="/images/btn-remove.svg" alt="Remove"/>
-       </div> 
+        </div> 
+       </div>
 
-      <div className="items">
-        <ul className="cartTotalBlock">
+      <div className="cartTotalBlock">
+        <ul>
           <li>
             <span>Итого:</span>
             <div></div>
             <b>5999 грн</b>
           </li>
           <li>
-            <span>Нфлог 5%:</span>
+            <span>Налог 5%:</span>
             <div></div>
             <b>5999 грн</b>
           </li>
         </ul>
+        <button className="greenButton">Оформить заказ <img src="/images/arrow.svg" alt="Arrow"/> </button>
       </div>
+      
 
 
       </div>
